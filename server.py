db64370b23df6e2cf824477fa54eb90d4c4ec802
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-25 21:04:47 -0400

    creating socket for server

--- server.py
+++ server.py
@@ -9,4 +9,9 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 
+# Creating a new socket (AF_INET argument  determines  family of # addresses that this
+# socket can  communicate with (in this case, Internet Protocol v4 addresses).
+# SOCK_STREAM is a transmission mechanism (in this case, two-way byte streams of data).
+# Anything that connects to the specified IP & port now, will hit that socket.
+server_socket = socket.socket(socket.AF_INET, sockets.SOCK_STREAM)
 
