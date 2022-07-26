e341492adac4e306089a976082e5799294c2d5f8
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-26 00:14:10 -0400

    creating client socket and connection to server

--- client.py
+++ client.py
@@ -8,3 +8,8 @@ ADDR = (SERVER, PORT)
 
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
+
+# Setting up the socket ofr the client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connecting to the server
+client.connect(ADDR)
