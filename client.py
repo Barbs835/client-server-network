49723b269a636398d40649276d1f327e16d0d2b5
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-26 00:13:04 -0400

    initial constants setup

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+
+# Setting up Server IP address and port constants
+PORT = 5050
+SERVER = 192.168.0.44
+ADDR = (SERVER, PORT)
+
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
