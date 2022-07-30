cab0f66ae8fc4374c0d02d2907d8682bbb704275
Barbara Surowiecka <bsurowie@gmail.com>
2022-07-30 01:37:54 -0400

    serialize the text as json xml binary

--- client_barb.py
+++ client_barb.py
@@ -13,11 +13,13 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "DISCONNECT"
+DISCONNECT_MESSAGE = "QUIT"
 HEADER = 1024
 crypt = Fernet('sqcyNL5kz2mxWb1KL2QSZWY-GCERE-scEgWBbvq9CCk=')
 dictionary = {}
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SEPARATOR = "<SEPARATOR>"
+
 
 def connect_to_server():
     try:
@@ -39,5 +41,68 @@ def client_disconnect():
         client_socket.close()
         print("Client has disconnected")
 
-connect_to_server()
-client_disconnect()
+def convert_dictionary_object(format):
+  """ Serialize the dictionary to JSON, XML, BINARY
+  """
+  if format == "BINARY":
+    with open('dictionary_object.bin', 'wb') as f:
+      pickle.dump(dictionary, f)
+      print('Dictionary serialised as BINARY. Filename: dictionary_object.bin')
+  elif format == "JSON":
+    with open('dictionary_object.json', 'w') as f:
+      json.dump(dictionary, f)
+      print('Dictionary serialised as JSON. Filename: dictionary_object.json')
+  elif format == "XML":
+    xml_content = dict2xml(dictionary, wrap="dictionary", indent="  ")
+    with open('dictionary_object.xml', 'w') as f:
+      f.write(xml_content)
+      f.close()
+      print('Dictionary serialised as XML. Filename: dictionary_object.xml')
+
+def send_file_to_server(file_name, encryption):
+        """ sned the file that contains either dictionary or text (if text, it could be
+        encrypted). Dictionary object is saved to either XML, JSON or BINary file
+
+        """
+        if encryption == True:
+            content_mode = "ENCRYPTED"
+            file_name = EncryptionHelper.encrypt_content(filename)
+            messageEncode = message.encode()
+            messageEncrypt = crypt.encrypt(messageEncode)
+            print("Message has been encrypted")
+        else:
+            content_mode = "NOT_ENCRYPTED"
+
+        # get the size of the file we want to send
+        file_size = os.path.getsize(filename)
+
+        client_socket.send(f"{file_name}{SEPARATOR}{file_size}{SEPARATOR}{content_mode}".encode())
+        # start sending the file
+        with open(file_name, "rb") as file:
+          while True:
+            bytes_read = file.read(HEADER)
+            if not bytes_read:
+              # transfer is done. nothing left to be sent
+              break
+              # Send what has been read so far
+              client_socket.sendall(bytes_read)
+              print(filename + ' was successfully sent to the server.')
+
+if __name__ == "__main__":
+    if connect_to_server():
+        # display command line prompt for client to key in a command
+        command = input("From the list of the available commands:\
+        \nhelp\nadd_dictionary\nadd_text\nQUIT\n type one of the commands")
+        while cmd != DISCONNECT_MESSAGE:
+            if command.lower() == "help":
+                # Show help menu
+                help_menu()
+            elif command.lower() == "add_dictionary":
+                pass
+        # TO DO ........................
+        # command line ask user to either add text entry or dictionary entry
+         # if users chooses text entry:
+        #ask if text should be encrypted or not, then save it as a file and transfer to server
+        # if user choses dictionary entry
+        # ask what format to serialise it to (JSON XML BINARY) then save it as a file and
+        # send it to the server
--- server_barb.py
+++ server_barb.py
@@ -2,7 +2,7 @@ import socket
 import threading
 from cryptography.fernet import Fernet
 import time
-
+from itertools import repeat , chain
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -10,7 +10,8 @@ ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 HEADER = 1024
 crypt = Fernet('sqcyNL5kz2mxWb1KL2QSZWY-GCERE-scEgWBbvq9CCk=')
-
+DISCONNECT_MESSAGE = "QUIT"
+SEPARATOR = "<SEPARATOR>"
 
 def server_start():
     """Set socket and start server to listen for connections"""
@@ -24,11 +25,11 @@ def server_start():
             # This line of code blocks the process by waiting for new connection to the server
             # When connection occurs server stores the client's "conn" socket object and
             # connection address
-            client_socket, client_addr = server.accept()
+            client_conn, client_addr = server.accept()
             print(f"Client at {client_addr} has connected to server.")
             # Passing the new connection socket object and address to the handle_client function
             # Each clinet connection will be handled in a new thread
-            thread = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+            thread = threading.Thread(target=handle_client, args=(client_conn, client_addr))
             thread.start()
             print("[STARTING] server is starting...")
             # Print the amount of client connections to the server (subtracting 1 as there is 1
@@ -38,18 +39,56 @@ def server_start():
             print("An established connection was aborted by the software in your host machine")
 
 
-def handle_client(client_socket, client_addr):
+def handle_client(client_conn, client_addr):
     connected = True
     while connected:
         msg = server.recv(HEADER).decode()
         if len(msg) == 0:
             return
-        if msg == 'EXIT':
-            print('Closing connection with client at: %s' % (address,))
-            # Freeing the socket
+        if msg == DISCONNECT_MESSAGE:
+            print(f"[DISCONNECTING] from client {client_addr}")
             server.close()
             break
+        else:
+            try:
+                # When clients sends file, the beginning of the streamed data will contain the
+                # name of the file, the size and whether it is encrypted. Hence, servers should
+                # extract this metadata first
+                file_name, file_size, encryption =  data.split(SEPARATOR)
+                filesize = int(file_size)
+                # Creating empty bytearray object which will hold received data in bytes.
+                received_data = bytearray()
+                # Looping collection of the HEADER size packets of data sent from client
+                while len(received_data) < file_size:
+                    data_packet = socket.recv(HEADER)
+                    received_data.extend(data_packet)
 
+                print('\nFile received: ' + file_name)
+                with open(filename, "wb") as f:
+                    # Save the file in the same directory as the program
+                    f.write(received_data)
+                    f.close()
+                    print('The file has been saved in the same directory folder')
+                    # Print content to the screen
+                    try:
+                        # Decode the byte array data into a readable format
+                        decoded_data = received_data.decode()
+                        print('Printing the content on the screen:\n')
+                        print(file_content_as_string)
+
+                        if content_type.strip() == 'ENCRYPTED':
+                            print('\nAttempting to decrypt encrypted data.')
+                            try:
+                                # Decrypt the file previously saved in
+                                # received_files folder
+                                # EncryptionHelper.decrypt_content(filename)
+                                pass
+                            except:
+                                print('Unable to decrypt. THere might be an issue with the encryption key.')
+                    except UnicodeDecodeError:
+                        print('Code cannot decode byte.')
+            except Exception as ex:
+                print("Unable to read the data")
 
 
 server_start()
