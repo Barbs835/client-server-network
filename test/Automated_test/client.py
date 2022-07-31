c3ef5a6bc86ec5e1c1437cbb9b52dc62384443ae
codelia00822 <108921369+codelia00822@users.noreply.github.com>
2022-08-01 00:57:27 +0800

    Add automated testing script

---
+++ test/Automated_test/client.py
@@ -0,0 +1,229 @@
+
+"""Client Side
+
+Run this file to create, serialize and send a dictionary
+and send a text file to the server.
+"""
+
+import socket
+import pickle
+import json
+
+from dict2xml import dict2xml
+from cryptography.fernet import Fernet
+from os.path import exists as file_exists
+
+# Key for fernet encryption
+#crupt_key = "R29kemlsbGFJc0p1c3RBSHVnZVRvYWRDYWxsZWRUaW0="
+client_crupt_key = "sqcyNL5kz2mxWb1KL2QSZWY-GCERE-scEgWBbvq9CCk="
+
+default_dictionary = {}
+
+
+# Functions for encrpytion of dictionary
+def encrypt(message: bytes):
+    """Encrypt the provided variable
+
+    Key arguments
+    message -- the value to be encrypted
+    """
+    return Fernet(client_crupt_key).encrypt(message)
+
+
+# Parse user input to make sure correct
+def user_input(choices):
+    """Validate User Input
+    Take user input and make sure
+    it matches what the program is
+    expecting
+
+    Key Arguments
+    choices -- Value the user inputs
+    """
+    found = "false"
+
+    while found == "false":
+        user_choice = input()
+        if(user_choice.lower() in choices):
+            return user_choice.lower()
+        else:
+            print("### Please choose a correct option ###")
+
+
+def check_file():
+    """Check file exists and filetype is correct"""
+    found = "false"
+
+    while found == "false":
+        path = input()
+        path_split = path.split(".")
+
+        if(file_exists(path) is True):
+            if(path_split[len(path_split) - 1] == "txt"):
+                print("### File found ###")
+                return path
+        if(path_split[len(path_split) - 1] != "txt"):
+            print("### Filetype is incorrect")
+            print("### Please enter your filename ending in .txt ###")
+        else:
+            print("### File does not exist ###")
+            print("### Please enter your filename ending in .txt ###")
+
+
+# Send data to server
+def send_data(serialized_data, PORT):
+    """Send the data to the server
+
+    Key arguments
+    serialized_data -- a string of serialized data
+    PORT -- Server port number eg. 5000
+    """
+    HOST = '127.0.0.1'  # Local machine
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(serialized_data.encode('latin1'))
+        data = s.recv(1024)
+        return "Data Sent"
+
+
+def dictionary_enter():
+    """Create a custom dictionary"""
+    global default_dictionary
+    value_amounts = ""
+    value_amounts = input("### How many values do you want to use? ###\n")
+    while not value_amounts.isnumeric():
+        value_amounts = input("### Please enter a number ###\n")
+    for v in range(int(value_amounts)):
+        dict_key = input("### Enter a key ###\n")
+        dict_value = input("### Enter a value ###\n")
+        default_dictionary[dict_key] = dict_value
+
+
+def serialize(default_dict, s_type):
+    """Serialize data
+
+    Key arguments
+    default_dict -- value to use as a default if other value not provided
+    s_type -- type of serilisation required
+    """
+    serialized = ""
+    method = ""
+    if(s_type == '1'):
+        serialized = pickle.dumps(default_dict)
+        method = "pickle"
+    if(s_type == '2'):
+        serialized = json.dumps(
+            default_dict, sort_keys=True, indent=4).encode()
+        method = "json"
+    if(s_type == '3'):
+        serialized = dict2xml(default_dict, wrap='root', indent="").encode()
+        method = "xml"
+    return serialized, method
+
+
+def parse_final_data(method, serialized, option):
+    """Concatenate Optional Variables
+
+    Key arguments
+    method -- Serialization method: Pickle Binary (1), Json (2), XML (3)
+    serialized -- the seralized data
+    option -- Output method: Print to Screen (1) or Save to File (2)
+    """
+    return method + '~' + str(serialized)[2:-1] + '~' + str(option)
+
+
+def main():
+    """Client Main Function
+    The starting point for execution for the programme.
+    """
+    global default_dictionary
+    """Serialize Section"""
+
+    print("### Serialization Section ###")
+    # Dictionary to send
+    print("### Do you wish to manually enter a dictionary? (Y) (N) ###")
+    choices = ["y", "n"]
+    d_type = user_input(choices)
+    if(d_type == "y"):
+        dictionary_enter()
+    else:
+        print("### Default Dictionary Used ###")
+        default_dictionary = {"key1": "value1", "key2": "value2"}
+
+    # Choose serialization type
+    print("### Please Choose Serialization Type ###")
+    print("### Pickle Binary (1), Json (2), XML (3) ###")
+    choices = ["1", "2", "3"]
+    s_type = user_input(choices)
+
+    # Output to screen or file
+    print("### Do you want to output to the",
+          "screen (1) or file (2) ###")
+    choices = ["1", "2"]
+    option = user_input(choices)
+
+    # Append to data for later review, seperated by ~
+    # Method~Serialized~Print/File
+    # e.g. json~{\n    "key1": "value1",\n    "key2": "value2"\n}~1
+
+    serialized, method = serialize(default_dictionary, s_type)
+
+    final_data = parse_final_data(method, serialized, option)
+
+    # Send data to server
+    send_data(final_data, 5000)
+    print("### Your data has been sent ###")
+
+    """File Section"""
+    print("\n### File Section ###")
+    # File choice and encrpytion
+    print("### Please enter your filename ending in .txt ###")
+
+    file_choice = check_file()
+
+    print("### Do you wish to locally encrypt your file? (Y) (N) ###")
+    choices = ["y", "n"]
+    enc_file_choice = user_input(choices)
+
+    # Output to screen or file
+    print("### Do you want to output the",
+          "file to the screen (1) or file (2) ###")
+    choices = ["1", "2"]
+    option = user_input(choices)
+
+    # Take in a read file
+    f = open(file_choice, "r")
+    file_contents = ""
+    for line in f:
+        file_contents += line
+
+    # Encrypt file contents (.txt files only contain text)
+    # Also adds user option at end if string
+    # e.g: ENCRYPTEDCONTENTS~1
+    enc_file_content = encrypt((file_contents + "~" + option).encode())
+
+    # Save encrypted version to disc
+    if(enc_file_choice == "y"):
+
+        # Create encrypted file name XXXX_enc.txt
+        # Changes forward slashes to backslashes
+        file_choice = file_choice.replace("/", "\\")
+        # Split on backslash
+        file_choice = file_choice.split("\\")
+        # Take last object from list
+        file_choice = file_choice[len(file_choice) - 1]
+        # Split on . to take just filename, no extension
+        split_name = file_choice.split(".")
+        enc_file_name = split_name[0] + "_enc.txt"
+
+        # Create file
+        file = open(enc_file_name, 'w')
+        file.write(enc_file_content.decode())
+        file.close()
+        print(f"{enc_file_name} created!")
+
+    send_data(enc_file_content.decode(), 5050)
+
+if __name__ == "__main__":
+    main()
---
+++ test/Automated_test/server.py
@@ -0,0 +1,166 @@
+
+""" Server Side
+Run this file to open the server.
+The server could process TXT , JSON, XML ,and files
+The server and the clients should work at the same machine
+This file created by Group C: Amr Ibrahim, Barbara Surowiecka, and Chun Wong
+"""
+
+import socket
+import pickle
+import json
+import sys
+import xmltodict
+
+# from dict2xml import dict2xml
+from cryptography.fernet import Fernet
+from os.path import exists as file_exists
+
+# Key for fernet encryption
+#crupt_key = "R29kemlsbGFJc0p1c3RBSHVnZVRvYWRDYWxsZWRUaW0="
+server_crupt_key = "sqcyNL5kz2mxWb1KL2QSZWY-GCERE-scEgWBbvq9CCk="
+
+""" Data from socket """
+in_data = ""
+
+#Function for decrpytion of dictionary
+def decrypt(token: bytes):
+    """decrpytion of dictionary based on the KEY"""
+    return Fernet(server_crupt_key).decrypt(token)
+
+# Starting the server at the defined port
+def start_server(PORT):
+    """Start up server
+
+    Keyword arguments:
+    PORT -- the desired port number eg. 5000 or 5050
+    """
+    global in_data
+    HOST = '127.0.0.1'  # The client and server should run at the same Local machine
+    time_out = 90       # The waiting time to receive input
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((HOST, PORT))
+        except:
+            print(f"Unable to bind to {HOST}:{PORT}")   # show the connection details to the user
+            sys.exit(1)
+        print(f"Server Open on {HOST}:{PORT}")
+        print("The server is ready to process data ...")
+        s.listen()                                      # Start to listen
+        s.settimeout(time_out)
+        conn, addr = s.accept()
+        with conn:
+            print(f"{addr[0]} Connected")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                in_data = repr(data)
+
+
+# Function to read the serialized data
+def serialized_receive():
+    """Receiving Serialized data"""
+
+    # Open server at port 5000
+    start_server(5000)
+    full_data = in_data[2:-1].split('~')
+
+    # Parse received data
+    message = full_data[1].replace('\\\\', '\\')
+    message = message.encode('utf-8')
+    message = message.decode('unicode-escape').encode('latin1')
+
+    # De-serialize data based on the data types
+    if (full_data[0] == "pickle"):
+        dict_ = pickle.loads(message)
+    if (full_data[0] == "json"):
+        dict_ = json.loads(message)
+    if (full_data[0] == "xml"):
+        dict_ = xml_deserialize(message)
+
+    # Output to screen or save to file
+    if (full_data[2] == "1"):
+        print(f"You provided the server with:\n{dict_}")
+    if (full_data[2] == "2"):
+        file_creator(str(dict_), "dictionary")
+
+
+# Function to process XML
+def xml_deserialize(message):
+    """De-Serialize a Serialized XML string
+
+    Keyword arguments:
+    message -- the string to de-serialize
+    """
+    msg_parsed = str(message)[2:-1]
+    msg_deserial = xmltodict.parse(msg_parsed)
+    s_dict = msg_deserial["root"]
+    try:
+        s_dict.pop('#text')
+        msg_dict = dict(s_dict)
+
+        return(msg_dict)
+    except KeyError:
+        return(s_dict)
+
+
+def file_receive():
+    """Receiving File"""
+
+    # Open server
+    start_server(5050)
+
+    full_data = in_data[2:-1]
+    plain_text = decrypt(full_data.encode('utf-8')).decode()
+    decrypt_text = plain_text.split("~")
+
+    if(decrypt_text[1] == "1"):
+        print(decrypt_text[0])
+    if(decrypt_text[1] == "2"):
+        file_creator(decrypt_text[0], "file")
+
+# Support function to create file
+def file_creator(content, type):
+    """Create file
+
+    Keyword arguments:
+    content -- content for the file as a string
+    """
+
+    file_num = 1
+    while(True):
+        filename = type + str(file_num) + ".txt"
+        # If file exists don't write, increment number
+        if(file_exists(filename) is False):
+            file = open(filename, 'w+')
+            file.write(content)
+            file.close()
+            print(f"{filename} created!")
+            return True
+
+        file_num += 1
+
+# The server main function
+def main():
+    """ Server Main Function
+    The starting point for execution for the programme.
+    """
+    global in_data
+    # Data from socket
+    in_data = ""
+
+    # For testing serialized_receive()
+    if len(sys.argv) > 1:
+        if(sys.argv[1] == "-T"):
+            for i in range(2):
+                serialized_receive()
+    else:
+        serialized_receive()
+        file_receive()
+
+
+if __name__ == "__main__":
+    main()
